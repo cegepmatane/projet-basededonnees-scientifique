facc266ed793cac1b138f99cd3337611bac0665c
math550 <math.nadeau@hotmail.com>
2017-09-08 11:45:09 -0400

    vue du panneau modifier

--- src/vue/PanneauModifierItem.java
+++ src/vue/PanneauModifierItem.java
@@ -6,11 +6,23 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Region;
 
 public class PanneauModifierItem extends Region
 {
+	
+	private TextField idBouee;
+    private TextField latitudeBouee;
+    private TextField longitudeBouee;
+    private TextField temperatureEauBouee;
+    private TextField temperatureAirBouee;
+    private TextField saliniteBouee;
+    private TextField vitesseVentBouee;
+    private TextField dimensionBouee;
+    private TextField pressionAtmospheriqueBouee;
+	
 	public PanneauModifierItem()
 	{
 		super();
@@ -26,6 +38,16 @@ public class PanneauModifierItem extends Region
 		grid.setPadding(new Insets(25, 25, 25, 25));
 		
 		Label labelTitreModifierItem = new Label("Modifier");
+		
+		idBouee = new TextField();
+        latitudeBouee = new TextField();
+        longitudeBouee = new TextField();
+        temperatureEauBouee = new TextField();
+        temperatureAirBouee = new TextField();
+        saliniteBouee = new TextField();
+        vitesseVentBouee = new TextField();
+        dimensionBouee = new TextField();
+        pressionAtmospheriqueBouee = new TextField();
 		
 		Button btnActionRetourEnArriere = new Button("Retour");
 		btnActionRetourEnArriere.setOnAction(new EventHandler<ActionEvent>() 
@@ -48,9 +70,27 @@ public class PanneauModifierItem extends Region
 		});
 		
 		grid.add(labelTitreModifierItem, 0, 0);
-		grid.add(btnActionRetourEnArriere, 0, 1);
-		grid.add(BtnActionSauvegardeeModification, 2, 1);
+		
+		addTextField(grid, idBouee, "Id de la bouée : ", 0, 1);
+        addTextField(grid, latitudeBouee, "Latitude : ", 0, 2);
+        addTextField(grid, longitudeBouee, "Longitude : ", 0, 3);
+        addTextField(grid, temperatureEauBouee, "Température de l'eau : ", 0, 4);
+        addTextField(grid, temperatureAirBouee, "Température de l'air : ", 0, 5);
+        addTextField(grid, saliniteBouee, "Salinité : ", 0, 6);
+        addTextField(grid, vitesseVentBouee, "Vitesse : ", 0, 7);
+        addTextField(grid, dimensionBouee, "Dimension : ", 0, 8);
+        addTextField(grid, pressionAtmospheriqueBouee, "Pression atmosphérique : ", 0, 9);
+		
+		grid.add(btnActionRetourEnArriere, 0, 10);
+		grid.add(BtnActionSauvegardeeModification, 1, 10);
 		
 		this.getChildren().add(grid);
 	}
+	
+	private void addTextField(GridPane grid, TextField textField,String texteLabel, int colonne, int ligne)
+	{
+        grid.add(new Label(texteLabel), colonne, ligne);
+        grid.add(textField, colonne+1, ligne);
+    }
+
 }
