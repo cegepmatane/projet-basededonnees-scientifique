ec6f1c22770b95ec1fe4d41f8b3e1be3342a1ee1
NeuroWine <scarpellino.giovanni@gmail.com>
2017-09-13 15:11:08 -0400

    Conversion vers un BoueeDAO en singleton

--- src/controleur/ActionAjouterBouee.java
+++ src/controleur/ActionAjouterBouee.java
@@ -1,6 +1,7 @@
 package controleur;
 
 import modele.Bouee;
+import modele.BoueeDAO;
 import vue.PanneauAjouterItem;
 import vue.PanneauListe;
 
@@ -38,48 +39,8 @@ public class ActionAjouterBouee {
         }
 
         Bouee bouee = new Bouee(latitude, longitude, temperatureEau, temperatureAir, salinite, vitesseVent, dimension, pressionAtmospherique);
-        Connection conn = null;
-        Statement stmt = null;
-        try{
-            //STEP 2: Register JDBC driver
-            Class.forName("com.mysql.jdbc.Driver");
-
-            //STEP 3: Open a connection
-            System.out.println("Connecting to database...");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/portmatane","root","");
-
-            //STEP 4: Execute a query
-            System.out.println("Creating statement...");
-            stmt = conn.createStatement();
-            String sql;
-            sql = "INSERT INTO `bouee` (`idBouee`, `latitude`, `longitude`, `temperatureEau`, `temperatureAir`, `salinite`, `vitesseVent`, `dimension`, `pressionAtmospherique`)" +
-                    " VALUES (NULL, '"+latitude+"', '"+longitude+"', '"+temperatureEau+"', '"+temperatureAir+"', '"+salinite+"', '"+vitesseVent+"', '"+dimension+"', '"+pressionAtmospherique+"');";
-            stmt.executeUpdate(sql);
-            stmt.close();
-            conn.close();
-            return true;
-        }catch(SQLException se){
-            //Handle errors for JDBC
-            se.printStackTrace();
-        }catch(Exception e){
-            //Handle errors for Class.forName
-            e.printStackTrace();
-            return false;
-        }finally{
-            //finally block used to close resources
-            try{
-                if(stmt!=null)
-                    stmt.close();
-            }catch(SQLException se2){
-            }// nothing we can do
-            try{
-                if(conn!=null)
-                    conn.close();
-            }catch(SQLException se){
-                se.printStackTrace();
-            }//end finally try
-        }//end try
-        return false;
+        BoueeDAO.getInstance().ajouterBouee(bouee);
+        return true;
     }
 
 }
---
+++ src/modele/BoueeDAO.java
@@ -0,0 +1,119 @@
+package modele;
+
+import javax.xml.transform.Result;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoueeDAO {
+
+    static final String DB_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/portmatane";
+    static final String DB_UTILISATEUR = "root";
+    static final String DB_MOTDEPASSE = "";
+
+    private static BoueeDAO instance = null;
+
+    public static BoueeDAO getInstance(){
+        if (instance == null) try {
+            instance = new BoueeDAO();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return instance;
+    }
+
+    private Connection connection;
+
+    public BoueeDAO() throws ClassNotFoundException, SQLException {
+        Class.forName(DB_DRIVER);
+        System.out.println("Connecting to database...");
+        connection = DriverManager.getConnection(DB_URL, DB_UTILISATEUR, DB_MOTDEPASSE);
+    }
+
+    public List<Bouee> recupererListeBouee() {
+        List<Bouee> listeBouees = new ArrayList<>();
+
+        System.out.println("Create statement...");
+        Statement stmt = null;
+        try {
+            stmt = connection.createStatement();
+            String sql = "SELECT * FROM bouee";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                int idBouee = rs.getInt("idBouee");
+                int latitude = rs.getInt("latitude");
+                int longitude = rs.getInt("longitude");
+                int temperatureEau = rs.getInt("temperatureEau");
+                int temperatureAir = rs.getInt("temperatureAir");
+                float salinite = rs.getInt("salinite");
+                float vitesseVent = rs.getInt("vitesseVent");
+                int dimension = rs.getInt("dimension");
+                float pressionAtmospherique = rs.getInt("pressionAtmospherique");
+
+                Bouee bouee = new Bouee(idBouee, latitude, longitude, temperatureEau, temperatureAir, salinite, vitesseVent, dimension, pressionAtmospherique);
+
+                listeBouees.add(bouee);
+            }
+            return listeBouees;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void supprimerBouee(int id) {
+        Statement stmt = null;
+        try {
+            stmt = connection.createStatement();
+            String sqlSupprimer = "DELETE FROM bouee WHERE idBouee = " + id;
+            stmt.executeUpdate(sqlSupprimer); //updateQuery
+            stmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void modifierBouee(Bouee bouee) {
+        Statement stmt = null;
+        try {
+            stmt = connection.createStatement();
+            String sql = "UPDATE `bouee` " +
+                    "SET `idBouee`=" + bouee.getIdBouee() + "," +
+                    "`latitude`=" + bouee.getLatitude() + "," +
+                    "`longitude`=" + bouee.getLongitude() + "," +
+                    "`temperatureEau`=" + bouee.getTemperatureEau() + "," +
+                    "`temperatureAir`=" + bouee.getTemperatureAir() + "," +
+                    "`salinite`=" + bouee.getSalinite() + "," +
+                    "`vitesseVent`=" + bouee.getVitesseVent() + "," +
+                    "`dimension`=" + bouee.getDimension() + "," +
+                    "`pressionAtmospherique`=" + bouee.getPressionAtmospherique() + " " +
+                    "WHERE `idBouee`= " + bouee.getIdBouee();
+            stmt.executeUpdate(sql);
+            stmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void ajouterBouee(Bouee bouee) {
+        Statement stmt = null;
+        try {
+            stmt = connection.createStatement();
+            String sqlAjouter = "INSERT INTO `bouee`(`idBouee`, `latitude`, `longitude`, `temperatureEau`, `temperatureAir`, `salinite`, `vitesseVent`, `dimension`, `pressionAtmospherique`) " +
+                    "VALUES (NULL,"+bouee.getLatitude()+"," +
+                    ""+bouee.getLongitude()+"," +
+                    ""+bouee.getTemperatureEau()+"," +
+                    ""+bouee.getTemperatureAir()+"," +
+                    ""+bouee.getSalinite()+"," +
+                    ""+bouee.getVitesseVent()+"," +
+                    ""+bouee.getDimension()+"," +
+                    ""+bouee.getPressionAtmospherique()+")";
+            stmt.executeUpdate(sqlAjouter);
+            stmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
